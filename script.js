2ef4dee18c2e957bd73168c73f36d4e8db3dd2d4
unknown <dsmallhoover@gmail.com>
2022-03-20 07:23:00 -0400

    centered image and added bootstrap

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var x = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-   document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+   document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
    if (distance < 0) {
        document.getElementById("demo").innerHTML = "TODAY IS THE DAY! " + "HAPPY BIRTHDAY QUINN ";
