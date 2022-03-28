2948faf8b2bea323f6de788a093872b1cddda29b
unknown <dsmallhoover@gmail.com>
2022-03-28 17:55:55 -0400

    changed date and pic for 2023

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const countDownDate = new Date("March 27, 2022 00:00:01").getTime();
+const countDownDate = new Date("March 27, 2023 00:00:01").getTime();
 var x = setInterval(function() {
     var now = new Date().getTime();
     var distance = countDownDate - now;
@@ -6,16 +6,15 @@ var x = setInterval(function() {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0
-    var yyyy = today.getFullYear();
-    today = mm + '/' + dd + '/' + yyyy;
+    var date = new Date().getDate();
+    var month = new Date().getMonth();
+    var bday = date + month;
 
    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-     if (today = 03 + '/' + 27) {
+     if (bday === 29) {
      document.getElementById("countdown").innerHTML = "TODAY IS THE DAY! " + "HAPPY BIRTHDAY QUINN ";
+     console.log("hi");
      }
 
 }, 1000);
